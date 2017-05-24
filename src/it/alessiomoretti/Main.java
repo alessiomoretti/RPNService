1357c4b87aac320eafbdd15abd49ef54a4d1411f
Alessio Moretti <alessio.moretti@live.it>
2017-05-24 15:47:56 +0200

    Add concurrent server thread class and server initialization in main.

--- src/it/alessiomoretti/Main.java
+++ src/it/alessiomoretti/Main.java
@@ -2,10 +2,19 @@ package it.alessiomoretti;
 
 import it.alessiomoretti.RPN.RPNCalculator;
 import it.alessiomoretti.RPN.RPNParser;
+import it.alessiomoretti.Server.ServerThread;
+
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(RPNCalculator.computeByStack(new RPNParser(args[0])));
+    public static void main(String[] args) throws IOException {
+        // TODO parametric port and exception handling
+        ServerSocket s = new ServerSocket(1234);
+        while(true) {
+            ServerThread thread = new ServerThread(s.accept());
+            thread.run();
+        }
     }
 }
---
+++ src/it/alessiomoretti/Server/ServerThread.java
@@ -0,0 +1,57 @@
+package it.alessiomoretti.Server;
+
+import it.alessiomoretti.RPN.RPNCalculator;
+import it.alessiomoretti.RPN.RPNParser;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class ServerThread implements Runnable {
+
+    private InputStream input = null;
+    private OutputStream output = null;
+    private Socket socket = null;
+
+    /**
+     * This class can be used to run a thread per request to the RPN service
+     * @param socket which is opened by the server binding
+     * @throws IOException
+     */
+    public ServerThread(Socket socket) throws IOException {
+        this.socket = socket;
+        this.input = socket.getInputStream();
+        this.output = socket.getOutputStream();
+    }
+
+    @Override
+    public void run() {
+        Reader reader = new InputStreamReader(this.input, StandardCharsets.US_ASCII);
+
+        try {
+            // accepting new chars from the socket
+            int inputChar;
+            StringBuffer inputBuffer = new StringBuffer();
+            while ((inputChar = reader.read()) != -1) {
+                char c = (char) inputChar;
+                // when ASCII linefeed is received the input is assumed complete
+                if (c == (int) '\n') break;
+                inputBuffer.append(c);
+            }
+
+            // building up the input string to pass to the RPNParser
+            String inputString = inputBuffer.toString();
+            // computing the result
+            Integer outputResult = RPNCalculator.computeByStack(new RPNParser(inputString));
+
+            // TODO Error messages
+            this.output.write((outputResult.toString() + "\n").getBytes(StandardCharsets.US_ASCII));
+            this.socket.close();
+
+        } catch (IOException e) {
+            // printing out on stderr the exception stacktrace
+            e.printStackTrace();
+        }
+
+    }
+}
